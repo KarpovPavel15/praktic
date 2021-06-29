2369c398c22671d5657a346c061f96a9627dc12f
github-VladOrpik <65450885+VladOrpik@users.noreply.github.com>
2021-06-29 15:01:19 +0300

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,41 @@
+
+ /* имя (постоянное)*/
+
+ const name ="Vlad";  
+ 
+ /*  фамилия (постоянное) */
+ 
+ const name ="Orpik";
+
+   
+  /* возраст (может изменяться) */
+ let age = "20";
+
+  /* 1
+  Что будет в консоле и почему:
+// 1
+console.log(test);
+var test = 'string';  
+
+Выведется значение стринг т.к переменная типа var
+
+//2
+var x = 'string';
+var x = 'string 2';
+console.log(x);
+
+Выведется string 2 т.к. string 2 было присвоенно позже
+
+// 3
+console.log(test);
+let test = 'string';
+
+var x = 'string';
+var x = 'string 2';
+console.log(x);
+
+let num = 12;
+let num = 1;
+
+Будет ошибка т.к. идентификатор num уже объявлен
+*/
