ec14ff33e1bb40944876fec960ed0421cb18b737
KarpovPavel15 <32909432+KarpovPavel15@users.noreply.github.com>
2021-06-29 16:32:38 +0300

    Update variables.js

--- variables.js
+++ variables.js
@@ -13,11 +13,11 @@
 
   /* 1
   Что будет в консоле и почему:
-// 1
+// 1 (Посмотреть про всплытие https://medium.com/@stasonmars/%D1%80%D0%B0%D0%B7%D0%B1%D0%B8%D1%80%D0%B0%D0%B5%D0%BC%D1%81%D1%8F-%D1%81-%D0%BF%D0%BE%D0%B4%D0%BD%D1%8F%D1%82%D0%B8%D0%B5%D0%BC-hoisting-%D0%B2-javascript-7d2d27bc51f1)
 console.log(test);
 var test = 'string';  
 
-Выведется значение стринг т.к переменная типа var
+Выведется значение стринг т.к переменная типа var)
 
 //2
 var x = 'string';
