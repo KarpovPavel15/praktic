547ccb35c7f30d6f6eeb1649704cc78f4d5a8371
github-VladOrpik <65450885+VladOrpik@users.noreply.github.com>
2021-06-29 15:04:20 +0300

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,14 @@
+/*  Создать объект с полем 'notebook' равным 'MacBook'
+Добавить в объект из предыдущей задачи поле 'price', равное 1500 и поле currensy, равная 'dollar'
+Добавить поле details, которое будет содержать объект с полями model и color (значения этих полей задайте сами)
+*/
+
+let stroe = {
+    notebook: 'MacBook',
+    price: 1500,
+    currency: 'dollar',
+    details ={
+        model= '1qw',
+        color = 'green'
+    }    
+}
